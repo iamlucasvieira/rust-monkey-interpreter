f07003e06dbd7f216e33a91d3299d8185d29b8ec
lucas <lucas6eng@gmail.com>
2024-04-09 22:48:57 +0200

    Format

--- src/builtins/mod.rs
+++ src/builtins/mod.rs
@@ -1,6 +1,6 @@
+use crate::object::{Error, Object};
 use anyhow::{self, Result};
 use std::rc::Rc;
-use crate::object::{Object, Error};
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Builtins {
@@ -8,7 +8,6 @@ pub enum Builtins {
 }
 
 impl Builtins {
-
     pub fn call(&self, args: Vec<Rc<Object>>) -> Result<Rc<Object>> {
         match self {
             Builtins::Len => builtin_len(args),
@@ -29,12 +28,19 @@ impl std::str::FromStr for Builtins {
 
 fn builtin_len(args: Vec<Rc<Object>>) -> Result<Rc<Object>> {
     if args.len() != 1 {
-        return Err(Error::TypeMismatch(format!("wrong number of arguments. got={}, want=1", args.len())).into());
+        return Err(Error::TypeMismatch(format!(
+            "wrong number of arguments. got={}, want=1",
+            args.len()
+        ))
+        .into());
     }
 
     match &*args[0] {
         Object::String(s) => Ok(Rc::new(Object::Integer(s.len() as i64))),
-        _ => Err(Error::TypeMismatch(format!("argument to `len` not supported, got {}", args[0].object_type())).into()),
+        _ => Err(Error::TypeMismatch(format!(
+            "argument to `len` not supported, got {}",
+            args[0].object_type()
+        ))
+        .into()),
     }
 }
-
